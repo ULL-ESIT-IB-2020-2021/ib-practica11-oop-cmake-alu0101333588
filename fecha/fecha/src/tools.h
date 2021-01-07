b5490fb76a0551718d1a17b004c12a77309fb335
alu0101333588 <alu0101333588@ull.edu.es>
2021-01-07 17:26:25 +0000

    Definitivo 07/01/2021

--- fecha/fecha/src/tools.h
+++
@@ -1,8 +0,0 @@
-
-#include <iostream>
-
-const std::string kHelpText = "Este programa determina si el año es bisiesto o no. \
-Además, imprime en el fichero de texto de salida. \
-Modo de uso: ./fechas dd/mm/aa N fichero_salida.txt";
-
-void Usage(int argc, char *argv[]);
